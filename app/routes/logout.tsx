d80ed24fb57ec283d0f13cf4993a329220b19c5f
Arnaud Fernandés <arnaud.fernandes@outlook.fr>
2022-09-14 07:38:56 +0300

    refactor: logout is now a GET

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,15 +1,7 @@
 import type { ActionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
 
 import { destroyAuthSession } from "~/modules/auth/session.server";
-import { assertIsPost } from "~/utils/http.server";
 
-export async function action({ request }: ActionArgs) {
-  assertIsPost(request);
-
+export async function loader({ request }: ActionArgs) {
   return destroyAuthSession(request);
-}
-
-export async function loader() {
-  return redirect("/");
 }
